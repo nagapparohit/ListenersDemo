f8d32a926dfec04b521995b3a47aa309eb0a03d2
rohit nagappa <rnagappa35@gmail.com>
2020-07-24 09:12:55 +0530

    implemented listenerd demo

---
+++ src/listeners/MyListener.java
@@ -0,0 +1,58 @@
+package listeners;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class MyListener
+ *
+ */
+public class MyListener implements HttpSessionListener, HttpSessionAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MyListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent arg0)  { 
+    	System.out.println("Hello from listener, session created at"+new Date(arg0.getSession().getCreationTime()));
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent arg0)  { 
+    	System.out.println("Hello from listenr, session Destroyed at"+new Date());
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent arg0)  { 
+    	System.out.println("from listener key with "+ arg0.getName()+" and value is "+arg0.getValue()+" added succefully.");
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent arg0)  { 
+    	System.out.println("from listener key with "+ arg0.getName()+" and value is "+arg0.getValue()+" removed succefully.");
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent arg0)  { 
+    	System.out.println("from listener key with "+ arg0.getName()+" and value is "+arg0.getValue()+" replaced succefully.");
+    }
+	
+}
